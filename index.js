165cb1ee230e9b202ea66c0c50f2ab0498e86086
Kane Mathis <admin@kanemathis.com>
2022-04-25 13:09:43 -0400

    commit tree 1cd11b3eef5f380b28fffb427939df0f596960ad

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+//document.addEventListener("DOMContentLoaded", submitData)
+
+
+function submitData(uName, uMail){
+    let createEl = document.createElement('div');
+    let formData = {
+        name:uName,
+        email:uMail,
+    }
+    return fetch('http://localhost:3000/users',
+    {
+        method: 'POST',
+    headers: {
+        'Content-Type':'application/json',
+        'Accept':'application/json',
+    },
+    body: JSON.stringify(formData)
+}).then(res => res.json())
+    .then(data => document.body.append(data["id"]))
+    .catch(function (error) {
+        document.querySelector('body').append('Unauthorized Access'); 
+        console.log(error.message)
+    })
+    
+   
+        
+    }
+   
